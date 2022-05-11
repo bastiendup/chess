95f759d92f3fce8f95fd1d935af5234f9c6cea19
Bastien DUPENLOUP <bastien.dupenloup@gmail.com>
2022-05-11 17:31:08 +0200

    add check for promotion, movement and checkmate move

--- chess_manager.py
+++ chess_manager.py
@@ -1,4 +1,3 @@
-from calendar import c
 import re
 
 
@@ -15,7 +14,7 @@ class chess_manager:
 
     def CheckPiece(movement):
         pieces = {'K': 'King', 'B': 'Bishop', 'Q': 'Queen', 'N': 'Knight', 'R': 'Rook'}
-        piece_regex = re.compile(r'^(K|Q|R|B|N)')
+        piece_regex = re.compile(r'^K|Q|R|B|N')
         match = re.search(piece_regex, movement)
         piece = 'Pawn'
         if match:
@@ -35,11 +34,11 @@ class chess_manager:
 
     def DisambiguatingFile(movement):
         # Check for the file of departure before the move
-        regex = re.compile(r'^[a-h](x|[a-h])')
+        regex = re.compile(r'^([a-h])(x|[a-h])')
         match = re.search(regex, movement)
         disambiguating_file = 'No'
         if match:
-            disambiguating_file = match.group()
+            disambiguating_file = match.group(1)
             print(f'Disambiguating File -> {disambiguating_file}')
             return re.sub(r'^[a-h]', '', movement) 
         print(f'Disambiguating File -> {disambiguating_file}')
@@ -47,11 +46,11 @@ class chess_manager:
 
     def DisambiguatingRank(movement):
         # Check for the rank of departure before the move
-        regex = re.compile(r'^[1-8](x|[a-h])')
+        regex = re.compile(r'^([1-8])(x|[a-h])')
         match = re.search(regex, movement)
         disambiguating_file = 'No'
         if match:
-            disambiguating_file = match.group()
+            disambiguating_file = match.group(1)
             print(f'Disambiguating Rank -> {disambiguating_file}')
             return re.sub(r'^[1-8]', '', movement) 
         print(f'Disambiguating Rank -> {disambiguating_file}')
@@ -59,17 +58,54 @@ class chess_manager:
 
     def DisambiguatingFileAndRank(movement):
         # Check for both the file and rank of departure before the move
-        regex = re.compile(r'^[a-h][1-8](x|[a-h])')
+        regex = re.compile(r'^([a-h][1-8])(x|[a-h])')
         match = re.search(regex, movement)
         disambiguating_file = 'No'
         if match:
-            disambiguating_file = match.group()
+            disambiguating_file = match.group(1)
             print(f'Disambiguating File & Rank -> {disambiguating_file}')
             return re.sub(r'^[a-h][1-8]', '', movement) 
         print(f'Disambiguating File & Rank -> {disambiguating_file}')
         return movement
 
+    def CheckMoving(movement):
+        # Check for move
+        regex = re.compile(r'^[a-h][1-8]')
+        match = re.search(regex, movement)
+        move = 'No'
+        if match:
+            move = match.group()
+            print(f'Moving -> {move}')
+            return re.sub(r'^[a-h][1-8]', '', movement)
+        print(f'Moving -> {move}')
+        return movement
 
+    def CheckPromotion(movement):
+        # Check for pawn promote
+        promote_piece = {'K': 'King', 'B': 'Bishop', 'Q': 'Queen', 'N': 'Knight', 'R': 'Rook'}
+        regex = re.compile(r'^=(Q|B|N|R)')
+        match = re.search(regex, movement)
+        promotion = 'No'
+        if match:
+            promotion = promote_piece[match.group(1)]
+            print(f'Promotion -> {promotion}')
+            return re.sub(r'^=(Q|B|N|R)', '', movement)
+        print(f'Promotion -> {promotion}')
+        return movement
+
+    def CheckForCheckMove(movement):
+        # Check for checkmate
+        check_ending = {'+': 'Check', '#': 'Checkmate'}
+        regex = re.compile(r'^(\+|\#)')
+        match = re.search(regex, movement)
+        checkmate = 'No'
+        if match:
+            checkmate = check_ending[match.group(1)]
+            print(f'Checkmate -> {checkmate}')
+            return re.sub(r'^\+|\#', '', movement) 
+        print(f'Checkmate -> {checkmate}')
+        return movement
+    
     if __name__ == '__main__':
         while True:
             movement = input('Movement :')
@@ -79,5 +115,7 @@ class chess_manager:
             movement = DisambiguatingRank(movement)
             movement = DisambiguatingFileAndRank(movement)
             movement = CheckCapture(movement)
-            print(f'Movement is now : {movement}')
+            movement = CheckMoving(movement)
+            movement = CheckPromotion(movement)
+            movement = CheckForCheckMove(movement)
             print('')
