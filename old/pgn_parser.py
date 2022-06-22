f0286b191831f0da47739943e25e5b126809b664
Bastien DUPENLOUP <bastien.dupenloup@gmail.com>
2022-06-22 17:53:37 +0200

    delete old code

--- old/pgn_parser.py
+++
@@ -1,5 +0,0 @@
-import chess
-
-png_folder = open('Viswanathan Anand_vs_Garry Kasparov_2021.07.10.pgn')
-current_game = chess.pgn.read_game(png_folder)
-png_text = str(current_game.mainline_moves())
--- pgn_reader.py
+++ pgn_reader.py
@@ -1,28 +1,21 @@
 import re
 import sys
 import time
-from typing import final
 
 from chess_manager import Chess_Manager
-
-pgn_game = ''
 
 with open('Viswanathan Anand_vs_Garry Kasparov_2021.07.10.pgn') as f:
     pgn_game = f.read()
 
-moves = re.sub(r'(\[.*\]\n\n?)', '', pgn_game, re.MULTILINE)
-clean_line_return = re.sub(r'\n', ' ', moves, 0, re.MULTILINE)
+clean_headers = re.sub(r'(\[.*\]\n\n?)', '', pgn_game, re.MULTILINE)
+clean_line_return = re.sub(r'\n', ' ', clean_headers, 0, re.MULTILINE)
 
-regex = r'(\d\d?\d?\. )'
+regex = r'(\d\d?\d?\. ?)'
 subst = '\\n'
 
 result = re.sub(regex, subst, clean_line_return, 0, re.MULTILINE)
 
 moves = re.split(r'\n', result)
-# for move in moves:
-#     if move == '':
-#         continue
-#     print(move)
 
 manager = Chess_Manager()
 print('\n')
@@ -31,7 +24,7 @@ for move in moves:
         continue
     for movement in move.split():
         manager.reset_turn()
-        time.sleep(.25)
+        time.sleep(.1)
         manager.is_white_turn = not manager.is_white_turn
         sys.stdout.write("\x1b[2K")  # Delete line before writing it : avoid having the previous move written
         color = 'white' if manager.is_white_turn else 'black'
@@ -41,10 +34,10 @@ for move in moves:
         # movement = input(f'White turn ? {manager.is_white_turn},  Movement : ')
         final_score = manager.check_final_score(movement)
         if final_score:
-            sys.stdout.write('\033[B' * 14)  #
+            sys.stdout.write('\033[B' * 14)  # Move cursor down 14 lines
             print(f'              {final_score}')
-
             break
+
         rook = manager.CheckIfKingRook(movement)
         if not rook:
             movement = manager.check_piece(movement)
